487ec220ff50f217f34cfeb9eeb5c07646ee510f
DuongAty <atyduongtran@gmail.com>
2023-05-19 01:27:24 +0700

    first commit

---
+++ src/server.js
@@ -0,0 +1,21 @@
+import express from "express";
+import bodyParser from "body-parser";
+import viewEngine from "./config/viewEngine";
+import initWedRoutes from "./route/wed";
+import { required } from "nodemon/lib/config";
+require('dotenv').config();
+
+
+let app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extends: true }))
+
+
+viewEngine(app);
+initWedRoutes(app);
+
+
+let port = process.env.PORT
+app.listen(port, () => {
+    console.log("Runing " + port)
+})
