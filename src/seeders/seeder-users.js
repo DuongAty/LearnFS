46df3efba867c5ef25c79e9702335a490d01080b
DuongAty <atyduongtran@gmail.com>
2023-05-20 02:42:20 +0700

    third

--- src/seeders/seeder-users.js
+++ src/seeders/seeder-users.js
@@ -4,9 +4,15 @@
 module.exports = {
   async up(queryInterface, Sequelize) {
     return queryInterface.bulkInsert('Users', [{
-      firstName: 'John',
-      lastName: 'Doe',
-      email: 'example@example.com',
+      email: 'admin@gmail.com',
+      password: '123456',
+      firstName: 'Duong',
+      lastName: 'Aty',
+      address: 'Dang Nang',
+      gender: 1,
+      typeRole: 'ROLE',
+      keyRole: 'R1',
+
       createdAt: new Date(),
       updatedAt: new Date()
     }]);
